ae043e7474c7aa047b8be0dda80e7479871a766d
aasanchezlazcano <adan.sanchez@dacodes.com.mx>
2021-11-17 15:59:04 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+
+    res.send('welcome');
+})
+
+app.listen(app.get('port'), _ => {
+    try {
+        console.log(`Server running on port ${app.get('port')}`);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
